8c508bbed90ae5b22fc9dc1c774952f63878bb97
Supreme Oghenewoakpo <supreme.oghenewoakpo@gmail.com>
2022-09-09 12:12:55 +0100

    C programming just begun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,8 @@
 /**
  * Main -Entry point
  *
+ * Description:  a C program that prints exactly "Programming is like building a multilingual puzzle
+ *
  * Return: Always 0 (Success)
  */
 int main(void)

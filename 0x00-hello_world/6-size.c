ff56304a3891701c23bb1a26f2721bb77833cc7a
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-09 13:59:10 -0700

    The size of various data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,14 +13,14 @@ int main(void)
 {
 	char c;
 	int i;
-	long int l;
-	long long int li;
+	long int li;
+	long long int lli;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }

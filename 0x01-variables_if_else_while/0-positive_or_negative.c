3e73d9115c3dc3e3996ef125d5ecd0f5491aa521
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-10 12:06:14 -0700

    if ....else if...if loop

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program will assign a
+ * random number to the variable n each time
+ * it is executed. Complete the source code in
+ * order to print whether the number stored in
+ * the variable n is positive or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int z;
+
+	srand(time(0));
+	z = rand() - RAND_MAX / 2;
+
+/* This is the loop over the random number */
+	if (z > 0)
+	{
+		printf("%d is positive \n", z);
+	}
+	else if (z < 0)
+	{
+		printf("%d is negative \n", z);
+	}
+	else
+	{
+		printf("%d is zero \n", z);
+	}
+
+	return (0);
+}

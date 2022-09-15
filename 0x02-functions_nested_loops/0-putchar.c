9bb933a535aafef4923f4d8bd1d1cb2f3aa2e4f4
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-14 19:59:23 -0700

    Prototye and header files usage

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_putchar - print _putchar and \n
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+
+
+	return (0);
+}

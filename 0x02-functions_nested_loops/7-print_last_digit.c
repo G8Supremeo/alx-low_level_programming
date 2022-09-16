ed746a2931ceb227774ace326df5a8e8760661b8
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-15 20:02:08 -0700

    Prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,19 +3,23 @@
 /**
  * print_last_digit - this function will print the last digit of a number
  *
- * @j: number whose last digit is to be printed
+ * @r: number whose last digit is to be printed
  *
  * Return: The value of the last digit
  */
 
-int print_last_digit(int j)
+int print_last_digit(int r)
 {
-	int last_digit = j % 10;
+	int last_digit;
+
+	last_digit = (r % 10);
 
 	if (last_digit < 0)
 	{
 		last_digit *= -1;
-		_putchar(last_digit + '0');
 	}
+
+	_putchar(last_digit + '0');
+
 	return (last_digit);
 }

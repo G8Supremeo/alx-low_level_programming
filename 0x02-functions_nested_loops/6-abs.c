877413e0469196e4e301fff2b21f21f5062e245e
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-15 19:19:21 -0700

    Computes the abs value of all int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of every int
+ *
+ * @e: int whose abs value to be determined
+ *
+ * Return: the abs value of the int
+ */
+
+int _abs(int e)
+{
+	if (e >= 0)
+	{
+		return (e);
+	}
+
+	else
+	{
+		return (-e);
+	}
+}

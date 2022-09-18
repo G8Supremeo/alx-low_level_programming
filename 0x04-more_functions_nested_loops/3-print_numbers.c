c1c04786ca17499f6e7355296a520cfefe0e492e
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-17 21:09:01 -0700

    Print the digit from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - this function prints the numbers, from 0 to 9
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int s;
+
+	for (s = '0'; s < ':'; s++)
+		_putchar(s);
+
+	_putchar('\n');
+}

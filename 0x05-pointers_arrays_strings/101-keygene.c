62281093427abf2ea6ccb8817b1385d2b102b9ce
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-22 05:39:09 -0700

    password generator

--- 0x05-pointers_arrays_strings/101-keygene.c
+++ 0x05-pointers_arrays_strings/101-keygene.c
@@ -1,23 +1,42 @@
+// C program for 101-crackme
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+// A function that randomly generates password of length X
+
 void randomPasswordGeneration(int X)
 {
+// Initialize a counter
 	int c = 0;
+
 	int randomizer = 0;
 
+	// Seeding the random-number generator with the current time 
+	// so thst the password will be different every time
+	
 	srand((unsigned int)(time(NULL)));
 
+	// Array of numbers
 	char numbers[] = "012356789";
+
+	// Array of small alphabelts
 	char letter[] = "abcdefghijklmnopqrstuvwxyz";
+
+	// Array of capital alphabets
 	char LETTER[] = "ABCWDEFXGHIYJKYLMSQTUZNOPRV";
+
+	// Array of special symbols
 	char symbols[] = "!@#$%^&\\*?>/<\"|/+-";
+
+	// Stores the random password
 	char password[X];
 
+	// To select the randomizer inside the loop
 	randomizer = rand() % 4;
 
+	// Iterate the counter over the range of 0 to X
 	for (c = 0; c < X; c++)
 	{
 		if (randomizer == 1)
@@ -47,10 +66,13 @@ void randomPasswordGeneration(int X)
 	}
 }
 
+// Driver code
 int main()
 {
+// Length of the password to be generated
 int X = 12;
 
+// Function call
 randomPasswordGeneration(X);
 printf("\n");
 return 0;

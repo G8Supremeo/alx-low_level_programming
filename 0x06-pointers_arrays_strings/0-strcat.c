05a3c363371c05ac9b6c766421dcb7d402d7defc
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-22 10:42:18 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@ char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
-	while (*(dest++))
+	while (dest[index++])
 	{
 		dest_len++;
 	}
@@ -19,6 +19,7 @@ char *_strcat(char *dest, char *src)
 	for (index = 0; src[index]; index++)
 	{
 		dest[dest_len++] = src[index];
+		dest_len++;
 	}
 	return (dest);
 }

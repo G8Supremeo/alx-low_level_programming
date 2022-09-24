4babc5a4062373f9942bf4d51896ddd2188a2ee7
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-09-24 01:18:41 -0700

    Print an integer with _putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,39 @@
 #include "main.h"
+void print_integer(int o);
 
 /**
-  * print_number - a function that prints out an integer
-  * @n: integer to be printed using _putchar.c
-  * Return: Always 0 (Success)
-  */
+ * print_number - a function that prints an integer
+ * @n: the integer to be printed using _putchar.c
+ * Return: Always 0 (Success)
+ *
+ */
 
 void print_number(int n)
 {
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		print_integer(n * -1);
 	}
-	else if (n / 10)
-	{
-		print_number(n / 10);
+	else
+		print_integer(n);
 	}
-	_putchar((n % 10) + '0');
+
+
+/**
+  * print_integer - a sub function to print the integer
+  * @o: an integer > || < 0
+  */
+
+void print_integer(int o)
+{
+	int i = 1000000000;
+
+	for (; i >= 1; i /= 10)
+		if (o / i != 0)
+		{
+			_putchar(((o / i) % 10) + '0');
+		}
 }

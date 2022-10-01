6147a4cfe39d2dc94af9323c87feae7fb40cf776
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-01 05:38:30 -0700

    Sub-String Search

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,16 +13,22 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
+	int j = 0;
 
-	for (; *haystack; haystack++)
+	while (needle[j] != '\0')
+		j++;
+
+	while (*haystack)
 	{
 		for (i = 0; needle[i]; i++)
 		{
-			if (*haystack == needle[i])
-				return (needle);
-
+			if (haystack[i] != needle[i])
+				break;
 		}
+		if (i != j)
+			haystack++;
+		else
+			return (haystack);
 	}
-
 	return (NULL);
 }

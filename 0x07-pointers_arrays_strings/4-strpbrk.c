35b5647f80afa9fa2f42533a6480a3a99c813f26
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-01 03:14:18 -0700

    String Search for any byte

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * _strpbrk - A function that searches a string for any of a set of bytes
+  * @s: string to be searched or scanned
+  * @accept: string containing the characters to match
+  *
+  * Return: A pointer to the byte in s that matches one of the bytes
+  * in accept or NULL if no such byte is found
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	for (; *s; s++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+				return (s);
+		}
+	}
+
+	return (NULL);
+}

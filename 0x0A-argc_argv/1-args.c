4697398ed791a9b539b5af47c9c318aaf659a215
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-04 04:20:03 -0700

    Prints argc - 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
-  * main - a program that prints its name, followed by a new line
-  * If the exec program is renamed, it will print the new name
-  * without having to compile the c file again
+  * main - a program that prints the number of arguments passed
+  * The program prints a number, followed by a new line
   * @argc: number of arguments passed into the program
   * @argv: array which holds the names of all the arguments
   * passed into the program

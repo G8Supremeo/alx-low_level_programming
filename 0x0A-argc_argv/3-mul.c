9c75897a7f3cfda8e4da07ce58dd4b30a23407c0
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-04 06:02:41 -0700

    Multiplies two numbers from the CLA

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,21 +18,20 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int Mul = 1;
+	int x, y, z;
 
-	if (argc >= 3)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			Mul *= atoi(argv[i]);
-		}
-		printf("%d\n", Mul);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return(1);
+	}
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		z = (x * y);
+
+		printf("%d\n", z);
 	}
 
 	return (0);

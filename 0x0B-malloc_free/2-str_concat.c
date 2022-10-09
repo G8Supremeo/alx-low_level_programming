5f9da7ddccd8ad8c51d4f7310bddbf0b47820028
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-08 17:12:20 -0700

    String Concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,26 +16,21 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k, l, m, n;
 
 	if (s1 == NULL)
-	{
 		i = 0;
-	}
 	else
-	{
 		for (i = 0; s1[i]; i++)
 			;
-	}
+
 	if (s2 == NULL)
-	{
 		j = 0;
-	}
 	else
 	{
 		for (j = 0; s2[j]; j++)
 			;
 	}
-	k = i + j + 1; n = j;
+	k = i + j + 1;
+	n = j;
 	mem = malloc(k * sizeof(char));
-
 	if (mem == NULL)
 	{
 		return (NULL);

d8f888453a6b600dcb2e099b01a12128cb47152f
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-12 23:28:15 -0700

    A new type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+  * init_dog - A function that initialize a variable of type struct dog
+  * @d: name of struct pointer
+  * @name: name of dog pointer
+  * @age: age of dog
+  * @owner: owner of dog pointer
+  *
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

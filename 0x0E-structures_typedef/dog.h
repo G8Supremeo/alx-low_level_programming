87bd1dd76adf9a2dda671d06e08b48c2b3de3e25
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-12 22:39:43 -0700

    A new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,26 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+
+
+/**
+  * struct dog - dog info
+  * @name: name of dog
+  * @owner: owner of dog
+  * @age: age of dog
+  *
+  */
+
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+
+
+
+
 
 
 

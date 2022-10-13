a321f9a7e885bfba11c2f2407fd170f8a003c3ae
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-13 02:01:59 -0700

    typedef for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-
-
 /**
   * struct dog - dog info
   * @name: name of dog
   * @owner: owner of dog
   * @age: age of dog
-  *
   */
 
 struct dog
@@ -18,14 +15,14 @@ struct dog
 	float age;
 };
 
-
-
+/**
+  * dog_t - typedef for struct dog
+  */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/* dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d); */
-
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */

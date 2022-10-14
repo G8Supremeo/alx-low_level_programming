8efadeaee54a2bebd81817c4ca4ad8b224d75c4b
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-14 02:43:57 -0700

    calc ops

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+  * op_add - adds two numbers
+  * @a: first number
+  * @b: second number
+  * Return: always 0
+  */
+
+int op_add(int a, int b)
+{
+	printf("%d\n", (a + b));
+	return (0);
+}
+
+/**                                                               
+
+    * op_add - adds two numbers                                     
+
+      * @a: first number                                              
+
+        * @b: second number                                             
+
+	  * Return: always 0                                              
+
+	    */                                                              
+
+                                                                  
+
+int op_add(int a, int b)                                          
+
+{                                                                 
+
+	        printf("%d\n", (a + b));                                  
+
+		        return (0);                                               
+
+			/**                                                               
+
+			    * op_add - adds two numbers                                     
+
+			      * @a: first number                                              
+
+			        * @b: second number                                             
+
+				  * Return: always 0                                              
+
+				    */                                                              
+
+			                                                                  
+
+			int op_add(int a, int b)                                          
+
+			{                                                                 
+
+				        printf("%d\n", (a + b));                                  
+
+					        return (0);                                               
+
+

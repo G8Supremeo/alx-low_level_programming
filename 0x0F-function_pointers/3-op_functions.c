64649f94b941c3c145cb91c0d001b701f2a1ca36
Supreme Oghenewoakpo <supreme.oghenewoakpo@gmail.com>
2022-10-14 11:16:15 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -13,48 +13,53 @@ int op_add(int a, int b)
 	return (0);
 }
 
-/**                                                               
+/**
+  * op_sub - sub of two numbers
+  * @a: first number
+  * @b: second number
+  * Return: always 0
+  */
 
-    * op_add - adds two numbers                                     
+int op_sub(int a, int b)
+{
+	printf("%d\n", (a - b));
+	return (0);
+}
 
-      * @a: first number                                              
+/**
+  * op_mul - multiply two numbers
+  * @a: first number
+  * @b: second number
+  * Return: always 0
+  */
 
-        * @b: second number                                             
+int op_mul(int a, int b)
+{
+	printf("%d\n", (a * b));
+	return (0);
+}
 
-	  * Return: always 0                                              
+/**
+  * op_div - division of two numbers
+  * @a: first number
+  * @b: second number
+  * Return: always 0
+  */
 
-	    */                                                              
+int op_div(int a, int b)
+{
+	printf("%d\n", (a / b));
+	return (0);
+}
 
-                                                                  
-
-int op_add(int a, int b)                                          
-
-{                                                                 
-
-	        printf("%d\n", (a + b));                                  
-
-		        return (0);                                               
-
-			/**                                                               
-
-			    * op_add - adds two numbers                                     
-
-			      * @a: first number                                              
-
-			        * @b: second number                                             
-
-				  * Return: always 0                                              
-
-				    */                                                              
-
-			                                                                  
-
-			int op_add(int a, int b)                                          
-
-			{                                                                 
-
-				        printf("%d\n", (a + b));                                  
-
-					        return (0);                                               
-
-
+/**
+  * op_mod - a modulus b
+  * @a: first number
+  * @b: second number
+  * Return: always 0
+  */
+int op_mod(int a, int b)
+{
+	printf("%d\n", (a % b));
+	return (0);
+}

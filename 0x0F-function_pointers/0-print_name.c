c24245652bca68ebedcdc924c799990186f67584
Supre Meo <supreme.oghenewoakpo@gmail.com>
2022-10-13 21:06:57 -0700

    print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 
 /**
   * print_name - A function that prints a name
@@ -6,5 +7,6 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name != NULL && f != NULL)
+		(*f)(name);
 }

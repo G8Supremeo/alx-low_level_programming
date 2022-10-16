cf1c28bce883ab19829c7076338717087162b500
Supreme Oghenewoakpo <supreme.oghenewoakpo@gmail.com>
2022-10-17 00:01:12 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
   * print_numbers - A variadic func that prints numbers followed by a new line
   * @separator: the string to be printed between numbers
   * @n: is the number of integers passed to the function
-  * Return: 0 1
+  * Return: void
   */
 
 void print_numbers(const char *separator, const unsigned int n, ...)

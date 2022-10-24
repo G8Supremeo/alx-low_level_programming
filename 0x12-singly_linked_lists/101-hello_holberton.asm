42a0ef450ba614c67a2c599f8822dc190bf86069
Supreme Oghenewoakpo <supreme.oghenewoakpo@gmail.com>
2022-10-24 13:50:29 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,12 @@
-c
+      global 	main
+	    extern 	printf
+
+	    section .text
+main:
+	    mov	rdi, format
+	    mov	rax, 0
+	    call	printf
+	    mov	rax, 0
+	    ret
+format:
+	    db "Hello, Holberton", 10, 0

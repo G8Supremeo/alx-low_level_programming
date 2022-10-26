bf4df19fb8fd71b78f1ded1b3c25bc22683d14b0
Supreme Oghenewoakpo <Supreme.Oghenewoakpo@gmail.com>
2022-10-25 21:52:05 -0700

    add a node at the beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,27 +16,31 @@ size_t _strlen(char *str)
 }
 
 /**
-  * add_node - A func that  adds a new node at the beginning of a list_t list.
+  * add_nodeint - A func that  adds a new node at the beginning of a list_t list.
   * @head: linked list_t list head pointer
-  * @str: string to be added
+  * @n: int
   * Return:the address of the new element, or NULL if it failed
   */
-list_t *add_node(list_t **head, const char *str)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	list_t *new_node;
-
-
-	new_node = malloc(sizeof(list_t));
+	listint_t *new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-		return (NULL);
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
 	{
-		free(new_node);
+		free (new_node);
 		return (NULL);
 	}
+	new_node->n = n;
 
-	new_node->len = _strlen(new_node->str);
+	if (head == NULL)
+	{
+		free (new_node);
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
 	new_node->next = (*head);
 	(*head) = new_node;
 

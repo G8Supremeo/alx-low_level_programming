345898fb409380736491b8043bcb500b3d8c910d
Supreme Oghenewoakpo <Supreme.Oghenewoakpo@gmail.com>
2022-10-27 11:30:07 -0700

    Delete at an index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		next_node = (*head)->next;
-		free(head);
+		free(*head);
 		*head = next_node;
 		return (1);
 	}
